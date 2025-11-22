add9a69622b39fcf0572d01800de5e64300044b4
Kwarthys <Kwarthys@gmail.com>
2025-11-22 11:08:34 +0100

    Added timer before AIThinking widget show

--- scripts/UIManagement/WidgetsManager.cs
+++ scripts/UIManagement/WidgetsManager.cs
@@ -6,10 +6,13 @@ public partial class WidgetsManager : Control
 {
     [Export] private Godot.Collections.Array<string> labels;
     [Export] private Godot.Collections.Array<Control> widgets;
+    [Export] private float aiWidgetDelayBeforeShow;
 
     private Dictionary<string, int> widgetIndexPerLabel = new();
 
     public static WidgetsManager Instance;
+
+    private Timer aiWidgetTimer = new();
 
     public override void _Ready()
     {
@@ -18,6 +21,17 @@ public partial class WidgetsManager : Control
         {
             widgets[i].Hide();
             widgetIndexPerLabel.Add(labels[i], i);
+        }
+    }
+
+    public override void _Process(double _dt)
+    {
+        if(aiWidgetTimer.counting == false)
+            return;
+        if(aiWidgetTimer.update(_dt))
+        {
+            // Timer has finished !
+            widgets[widgetIndexPerLabel["AIThinking"]].Show();
         }
     }
 
@@ -34,11 +48,49 @@ public partial class WidgetsManager : Control
     private void doHide(string _label)
     {
         widgets[widgetIndexPerLabel[_label]].Hide();
+        aiWidgetTimer.interrupt();
     }
 
     private void doShow(string _label)
     {
-        widgets[widgetIndexPerLabel[_label]].Show();
+        if(_label == "AIThinking")
+        {
+            aiWidgetTimer.start(aiWidgetDelayBeforeShow);
+        }
+        else
+            widgets[widgetIndexPerLabel[_label]].Show();
+    }
+
+}
+
+public class Timer
+{
+    public bool counting {get; private set;} = false;
+    private double timeAccumulator = 0.0;
+    private double targetTime = 1.0;
+
+    public void start(double _targetTime)
+    {
+        targetTime = _targetTime;
+        timeAccumulator = 0.0;
+        counting = true;
+    }
+
+    public bool update(double _dt)
+    {
+        timeAccumulator += _dt;
+
+        if(timeAccumulator > targetTime)
+        {
+            counting = false;
+            return true;
+        }
+        return false;
+    }
+
+    public void interrupt()
+    {
+        counting = false;
     }
 
 }
